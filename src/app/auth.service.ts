19d647ddf3ac1ed298dad42cbd450e50c8ae7e30
zhengziyi <1473498140@qq.com>
2021-01-15 08:44:49 +0800

    jiemian

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
 export class AuthService {
     isLoggedIn = false;
     currentUser = 'zwj';
-    identity = '';
+    identity = 'user';
     login() {
         this.isLoggedIn = true;
     }
